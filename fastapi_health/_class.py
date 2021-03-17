42e46f4eff1dda791b2e2635adeb29e6f8f139d5
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-03-17 23:57:33 +0100

    ✨ Support different outputs and status code

---
+++ fastapi_health/_class.py
@@ -0,0 +1,34 @@
+from inspect import Parameter, Signature
+from typing import Any, Callable, Dict, List, Optional
+
+from fastapi import Depends
+from fastapi.responses import JSONResponse
+
+
+def health(
+    conditions: List[Callable[..., Any]],
+    *,
+    success_output: Optional[Dict[str, Any]] = None,
+    failure_output: Optional[Dict[str, Any]] = None,
+    success_status: int = 200,
+    failure_status: int = 503,
+):
+    async def endpoint(**dependencies):
+        values = [dependency for dependency in dependencies.values()]
+        output = dict(filter(lambda x: isinstance(x, dict), values))
+        if all(values):
+            return JSONResponse(success_output or output, status_code=success_status)
+        return JSONResponse(failure_output or output, status_code=failure_status)
+
+    params = []
+    for condition in conditions:
+        params.append(
+            Parameter(
+                f"param_{condition.__name__}",
+                kind=Parameter.POSITIONAL_OR_KEYWORD,
+                annotation=bool,
+                default=Depends(condition),
+            )
+        )
+    endpoint.__signature__ = Signature(params)
+    return endpoint
