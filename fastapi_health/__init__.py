1bf9ddb98713384dd653a507daef94cb03f80ef0
Marcelo Trylesinski <marcelo@recital.ai>
2021-08-11 08:33:01 +0200

    fix

--- fastapi_health/__init__.py
+++ fastapi_health/__init__.py
@@ -1,4 +1,4 @@
 from fastapi_health.route import health
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __all__ = ["health"]
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,4 @@ import fastapi_health
 
 
 def test_version():
-    assert fastapi_health.__version__ == "0.3.0"
+    assert fastapi_health.__version__ == "0.3.1"
