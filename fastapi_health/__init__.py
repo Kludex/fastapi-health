c03c62c55c242d39d0db9bad33f45d1f2f80dfc5
Marcelo Trylesinski <marcelo@recital.ai>
2021-08-11 08:28:06 +0200

    :arrow_up: Upgrade FastAPI dependency range

--- fastapi_health/__init__.py
+++ fastapi_health/__init__.py
@@ -1,5 +1,6 @@
-__version__ = "0.1.0"
+import importlib.metadata as importlib_metadata
 
 from fastapi_health.route import health
 
+__version__ = importlib_metadata.version(__name__)
 __all__ = ["health"]
