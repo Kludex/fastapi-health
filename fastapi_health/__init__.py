4eddc8528618a3d9d4184083c16680bee3e9248b
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-08-20 09:23:47 +0200

    🔖 Release 0.4.0 (#6)
    * 🔖 Release 0.4.0
    
    * Remove test
    
    * Add poetry pre release

--- fastapi_health/__init__.py
+++ fastapi_health/__init__.py
@@ -1,4 +1,5 @@
 from fastapi_health.route import health
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
+
 __all__ = ["health"]
--- tests/test_version.py
+++
@@ -1,5 +0,0 @@
-import fastapi_health
-
-
-def test_version():
-    assert fastapi_health.__version__ == "0.3.1"
