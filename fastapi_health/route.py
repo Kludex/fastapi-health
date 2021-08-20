a94c1ef2e0b8563dd387b05af39e9b83b5aed65a
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-08-20 08:33:12 +0200

    ✨ Support different outputs and status code (#2)
    * ✨ Support different outputs and status code
    
    * fix issue on dict
    
    * 📝 Document new setup
    
    * ✨ Implement handlers
    
    * Fix small details

--- fastapi_health/route.py
+++ fastapi_health/route.py
@@ -1,20 +1,43 @@
 from inspect import Parameter, Signature
-from typing import Callable, List
+from typing import Any, Awaitable, Callable, Dict, List, Union
 
-from fastapi import Depends, Response
+from fastapi import Depends
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 
-def health(conditions: List[Callable]):
-    def endpoint(**dependencies):
-        if all([dependency for dependency in dependencies.values()]):
-            return Response(status_code=200)
-        return Response(status_code=503)
+async def default_handler(**kwargs) -> Dict[str, Any]:
+    output = {}
+    for value in kwargs.values():
+        if isinstance(value, dict):
+            output.update(value)
+    return output
+
+
+def health(
+    conditions: List[Callable[..., Union[Dict[str, Any], bool]]],
+    *,
+    success_handler: Callable[..., Awaitable] = default_handler,
+    failure_handler: Callable[..., Awaitable] = default_handler,
+    success_status: int = 200,
+    failure_status: int = 503,
+):
+    async def endpoint(**dependencies):
+        if all(dependencies.values()):
+            handler = success_handler
+            status_code = success_status
+        else:
+            handler = failure_handler
+            status_code = failure_status
+
+        output = await handler(**dependencies)
+        return JSONResponse(jsonable_encoder(output), status_code=status_code)
 
     params = []
     for condition in conditions:
         params.append(
             Parameter(
-                f"param_{condition.__name__}",
+                f"{condition.__name__}",
                 kind=Parameter.POSITIONAL_OR_KEYWORD,
                 annotation=bool,
                 default=Depends(condition),
